95682582b52ac325dfd90b1d8bf5295aeadebe5b
ryuji <ryujispjp@gmail.com>
2022-01-04 17:47:00 +0900

    tableViewで設定画面作成する 酒井さんの動画でAPI連携の勉強する

---
+++ Bitcoin/SettingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingViewController.swift
+//  Bitcoin
+//
+//  Created by 髙橋　竜治 on 2022/01/04.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
