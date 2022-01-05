6c762f57107db14de2f2d67432775c6ff029ec94
ryuji <ryujispjp@gmail.com>
2022-01-05 19:01:04 +0900

    tableView追加 API取得確認する

---
+++ Bitcoin/Entity.swift
@@ -0,0 +1,13 @@
+//
+//  Entity.swift
+//  Bitcoin
+//
+//  Created by 髙橋　竜治 on 2022/01/05.
+//
+
+struct BTCInfo: Codable {
+
+    struct Total: Codable {
+        var price: Int
+    }
+}
--- Bitcoin/SettingViewController.swift
+++ Bitcoin/SettingViewController.swift
@@ -7,8 +7,25 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UITableViewController {
+
+    @IBOutlet weak var APITextField: UITextField!
+
+    @IBOutlet weak var priceTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func saveButton(_ sender: UIBarButtonItem) {
+        if let API = APITextField.text,
+           let price = priceTextField.text {
+            print(API)
+            print(price
+            )
+        }
+    }
+    @IBAction func toBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
